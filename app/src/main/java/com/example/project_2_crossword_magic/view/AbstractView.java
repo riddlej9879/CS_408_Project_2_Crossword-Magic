e40577aa25cc14eaaf5d7cdc10172fdc08d0524b
Home User <riddlej9879@yahoo.com>
2024-04-06 17:48:30 -0500

    Initial commit.

---
+++ app/src/main/java/com/example/project_2_crossword_magic/view/AbstractView.java
@@ -0,0 +1,9 @@
+package com.example.project_2_crossword_magic.view;
+
+import java.beans.PropertyChangeEvent;
+
+public interface AbstractView {
+
+    public abstract void modelPropertyChange(final PropertyChangeEvent evt);
+
+}
